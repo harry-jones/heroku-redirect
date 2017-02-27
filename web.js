336b966183b625ed321a48a9e63de0c52b4e243e
Harry Jones <harry.jones@mac.com>
2017-02-27 15:36:41 +0000

    update node and express

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 app.get('*', function(request, response) {
   response.redirect(301, process.env.NEW_BASE_URL + request.url)
